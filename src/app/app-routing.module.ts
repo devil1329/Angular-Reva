cb4b82633fa4bdce65e0430a2797bd5c9ee5a0c9
suvi2913 <server.visu143@gmail.com>
2023-03-18 01:11:44 +0530

    added footer and contact us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginRegistrationComponent } from './login-registration/login-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "LoginRegistration", component:LoginRegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginRegistrationComponent } from './login-registration/login-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LoginRegistrationComponent,
   ],
   imports: [
     BrowserModule,
